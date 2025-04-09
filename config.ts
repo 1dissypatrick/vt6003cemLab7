2f097047fed72f4fd51ae02d086a0dd86a0d66f1
ttt <rt@gmail.com>
2025-04-09 11:48:16 +0800

    initial commit

---
+++ config.ts
@@ -0,0 +1,12 @@
+
+
+export const config = {
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: 'super',
+  database: "postgres",
+  connection_limit: 100
+}
+
+// https://www.postgresql.org/download/windows/
